ba8c4e1bcad364ac55464cf6cfacb8d8185aa6a0
wta1ss <38648609+wta1ss@users.noreply.github.com>
2018-11-19 18:00:47 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,20 @@
+function getFileName() {
+  return window.location.href.split('/').pop();
+}
+
+var filename = getFileName();
+var opt;
+if(filename === 'other.html') {
+  opt = document.querySelector('option[value="other.html"]');
+}else {
+  opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected = true;
+
+document.getElementById('form').select.onchange = function() {
+  location.href = document.getElementById('form').select.value;
+}
+
 var separate_time = function(time) {
   var sec   = Math.floor((time / 1000) % 60);
   var min   = Math.floor((time / 1000 / 60) % 60);
@@ -25,20 +42,3 @@ var refresh = function() {
 }
 
 Update();
-
-function getFileName() {
-  return window.location.href.split('/').pop();
-}
-
-var filename = getFileName();
-var opt;
-if(filename === 'other.html') {
-  opt = document.querySelector('option[value="other.html"]');
-}else {
-  opt = document.querySelector('option[value="index.html"]');
-}
-opt.selected = true;
-
-document.getElementById('form').select.onchange = function() {
-  location.href = document.getElementById('form').select.value;
-}
