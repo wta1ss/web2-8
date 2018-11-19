cdf842f237616b4cd319847a4390a4c96f4c7f7c
wta1ss <38648609+wta1ss@users.noreply.github.com>
2018-11-19 17:39:08 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -25,3 +25,20 @@ var refresh = function() {
 }
 
 Update();
+
+function getFileName() {
+  return window.location.href.split('/').pop();
+}
+
+var filename = getFileName();
+var opt;
+if(filename === 'other.html') {
+  opt = document.querySelector('option[value="other.html"]');
+}else {
+  opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected = true;
+
+document.getElementById('form').select.onchange = function() {
+  location.href = document.getElementById('form').select.value;
+}
