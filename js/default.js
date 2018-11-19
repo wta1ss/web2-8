93cf732e605c8943594b7ee562470158abc0156b
wta1ss <38648609+wta1ss@users.noreply.github.com>
2018-11-19 16:33:28 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,27 @@
+var separate_time = function(time) {
+  var sec   = Math.floor((time / 1000) % 60);
+  var min   = Math.floor((time / 1000 / 60) % 60);
+  var hours = Math.floor((time / 1000 / 60 / 60) % 24);
+  var days  = Math.floor(time / 1000 / 60 / 60 / 24);
+  return [sec, min, hours, days];
+}
 
+var Update = function() {
+  var now = new Date();
+  var target = new Date(2020,7,24,0,0,0,0);
+  var diff = target.getTime() - now.getTime();
+  var counter = separate_time(diff);
+  document.getElementById('countdown').textContent =
+    '東京オリンピックまであと　' +
+    counter[3] + '日' +
+    counter[2] + '時' +
+    counter[1] + '分' +
+    counter[0] + '秒';
+  refresh();
+}
+
+var refresh = function() {
+  setTimeout(Update,1000);
+}
+
+Update();
